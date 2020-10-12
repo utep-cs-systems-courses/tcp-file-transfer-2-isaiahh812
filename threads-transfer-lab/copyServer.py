157fe9425f96a209900e8de8d61912751f0c0882
isaiahh812 <43229691+isaiahh812@users.noreply.github.com>
2020-10-11 19:42:15 -0600

    Add files via upload

---
+++ threads-transfer-lab/copyServer.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python3
+
+import sys
+
+sys.path.append("../lib")  # for params
+import re, socket, params, os
+
+switchesVarDefaults = (
+    (('-l', '--listenPort'), 'listenPort', 50001),
+    (('-d', '--debug'), "debug", False),  # boolean (set if present)
+    (('-?', '--usage'), "usage", False),  # boolean (set if present)
+)
+
+progname = "echoserver"
+paramMap = params.parseParams(switchesVarDefaults)
+
+debug, listenPort = paramMap['debug'], paramMap['listenPort']
+
+if paramMap['usage']:
+    params.usage()
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # listener socket
+bindAddr = ("127.0.0.1", listenPort)
+lsock.bind(bindAddr)
+lsock.listen(5)
+print("listening on:", bindAddr)
+os.chdir("ServerFiles")
+
+from threading import Thread;
+from encapFramedSock import EncapFramedSock
+
+class Server(Thread):
+    def __init__(self, sockAddr):
+        Thread.__init__(self)
+        self.sock, self.addr = sockAddr
+        self.fsock = EncapFramedSock(sockAddr)
+    def run(self):
+        while True:
+            file = (self.fsock.receive(debug)).decode()
+            fileName, fileSize = file.split(":")
+            fileSize = int(fileSize)
+
+            with open(fileName, "w+b") as newFile:
+                count = 0
+                while count < fileSize:
+                    payload = self.fsock.receive(debug)
+                    data = bytes(payload)
+
+                    if payload == b'':
+                        break
+
+                    newFile.write(data)
+                    count += 1
+            newFile.close()
+            if debug: print("rec'd: ", payload)
+            if payload == b'':
+                print("The file was received")
+            sys.exit(0)
+while True:
+    sockAddr = lsock.accept()
+    server = Server(sockAddr)
+    server.start()
